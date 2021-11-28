6eaccc229d16f66b18a260269f4797bcdf3df9a5
xeno-john <ionut.alexandru.baltariu@gmail.com>
2021-11-28 22:04:34 +0200

    Add initial module for sqlalchemy classes generation
    TODO: add imports, separate file for db connection, etc.

--- create_db_and_tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE generated_db;
-USE generated_db;
-CREATE TABLE `Books` (
-	`isbn` varchar(100) NOT NULL, 
-	`title` varchar(100) NOT NULL, 
-	`year_of_publishing` int(11) NOT NULL, 
-	PRIMARY KEY (`isbn`), 
-	UNIQUE KEY `books_un_1` (`title`),
-)
