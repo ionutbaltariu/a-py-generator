f1fbc164fbff8b34cc5720be1091f911a8720a96
xeno-john <ionut.alexandru.baltariu@gmail.com>
2021-12-07 20:56:23 +0200

    Modify test back to make pipeline succeed

--- src/test/test_dummy.py
+++ src/test/test_dummy.py
@@ -4,7 +4,7 @@ import unittest
 class TestDummy(unittest.TestCase):
 
     def test_sum(self):
-        self.assertEqual(sum([1, 2, 2]), 6, "Should be 6")
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
 
 if __name__ == '__main__':
