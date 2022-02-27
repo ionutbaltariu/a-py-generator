fe36519c95d90e67ecae849a5eadeebf6dd2ce64
xeno-john <ionut.alexandru.baltariu@gmail.com>
2022-02-27 20:41:46 +0200

    Use jinja2 for sql and db_conn generation - successfull

--- src/fastapi_crud_generator/sql_generator/generate_creation_script.py
+++ src/fastapi_crud_generator/sql_generator/generate_creation_script.py
@@ -1,6 +1,9 @@
 import json
 import logging
 from pathlib import Path
+from dataclasses import dataclass
+from typing import List
+from jinja2 import Template
 
 
 def get_project_root() -> Path:
@@ -15,6 +18,51 @@ datatype_converter = {
 }
 
 
+@dataclass
+class Field:
+    name: str
+    type: str
+    nullability: str
+
+    def get_sql_datatype_from_field(self, field: dict):
+        """
+        Method that translates a resource type into an SQL type (MariaDB).
+
+        :param field: The field of which type is to be translated
+        """
+        field_type = field["type"]
+
+        if field_type == 'string':
+            self.type = datatype_converter[field_type].format(length=field["length"])
+        else:
+            self.type = datatype_converter[field_type]
+
+    def get_sql_nullability(self, is_nullable: bool):
+        """
+        Method that returns the code to specify the nullability of a table field.
+        """
+        self.nullability = 'NOT NULL' if not is_nullable else ''
+
+    def __init__(self, field: dict):
+        self.name = field["name"]
+        self.get_sql_datatype_from_field(field)
+        self.get_sql_nullability(field["nullable"])
+
+
+@dataclass
+class Unique:
+    name: str
+    unique_fields: List[str]
+
+
+@dataclass
+class Table:
+    name: str
+    fields: List[Field]
+    uniques: List[Unique]
+    primary_key: str
+
+
 def generate_db_create_code(resources: str, path: str = f'{get_project_root()}/generated/') -> None:
     """
     Method that generates SQL code based on a given List of resources.
@@ -23,90 +71,28 @@ def generate_db_create_code(resources: str, path: str = f'{get_project_root()}/g
     :param path: [out] The path in which the code will be generated.
     """
     logging.info(f"Entered {generate_db_create_code.__name__}")
-    script_string = create_database_and_use_it()
     resources_dict = json.loads(resources)
+    tables_to_be_created = []
 
     for resource in resources_dict:
-        script_string += f'CREATE TABLE `{resource["table_name"]}` (\n'
-        script_string += generate_table_fields(resource["fields"])
-        script_string += generate_primary_key(resource["primary_key"])
-        if resource["uniques"] is not None:
-            script_string += generate_unique_keys(resource["uniques"])
-        script_string += ');\n\n'
+        temp_fields = []
+        temp_uniques = []
 
-    script_string = script_string.replace(',\n)', '\n)')
+        for field in resource['fields']:
+            temp_fields.append(Field(field))
 
-    with open(f'{path}/create_db_and_tables.sql', 'w', encoding='utf-8') as f:
-        f.write(script_string)
-        logging.info(f"Successfully created SQL script at path `{path}`.")
+        for unique in resource['uniques']:
+            temp_uniques.append(Unique(**unique))
 
+        tables_to_be_created.append(Table(name=resource['table_name'],
+                                          fields=temp_fields,
+                                          uniques=temp_uniques,
+                                          primary_key=resource['primary_key']))
 
-def generate_table_fields(fields: dict) -> str:
-    """
-    Method that generates the fields of an SQL table.
+    with open(f'{get_project_root()}/templates/sql.jinja2', 'r') as f:
+        sql_template = Template(f.read(), trim_blocks=True)
+        sql_code = sql_template.render(tables=tables_to_be_created)
 
-    :param fields: a Dictionary representing the fields of a resource
-    :return: The string equivalent of the table's fields.
-    """
-    script_string = ''
-
-    for field in fields:
-        script_string += f'\t`{field["name"]}` {get_sql_datatype_from_field(field)} ' \
-                         f'{get_sql_nullability(field["nullable"])}, \n'
-
-    return script_string
-
-
-def generate_primary_key(primary_key_field: str) -> str:
-    """
-    Method that generates a primary key constraint with the input field.
-    """
-    return f'\tPRIMARY KEY (`{primary_key_field}`),\n'
-
-
-def generate_unique_keys(unique_keys: dict) -> str:
-    script_string = ''
-
-    for unique_key in unique_keys:
-        script_string = f'\tUNIQUE KEY `{unique_key["name"]}` ('
-        unique_fields = unique_key["unique_fields"]
-        unique_fields_tokens = [f'`{token}`' for token in unique_fields]
-        script_string += ', '.join(unique_fields_tokens)
-
-    script_string += '),\n'
-
-    return script_string
-
-
-def create_database_and_use_it(database_name: str = 'generated_db') -> str:
-    """
-    Method that generates the SQL code that creates a database and uses it.
-    """
-    return f'CREATE DATABASE {database_name};' \
-           f'\n' \
-           f'USE {database_name};' \
-           f'\n'
-
-
-def get_sql_datatype_from_field(field: dict) -> str:
-    """
-    Method that translates a resource type into an SQL type (MariaDB).
-
-    :param field: The field of which type is to be translated
-    :return: The equivalent MariaDB type for the field.
-    """
-    field_type = field["type"]
-
-    if field_type == 'string':
-        datatype = datatype_converter[field_type].format(length=field["length"])
-    else:
-        datatype = datatype_converter[field_type]
-
-    return datatype
-
-
-def get_sql_nullability(is_nullable: bool):
-    """
-    Method that returns the code to specify the nullability of a table field.
-    """
-    return 'NOT NULL' if not is_nullable else ''
+        with open(f'{path}/create_db_and_tables.sql', 'w', encoding='utf-8') as gen_f:
+            gen_f.write(sql_code)
+            logging.info(f"Successfully created SQL script at path `{path}`.")
--- src/fastapi_crud_generator/sqlalchemy_code_generator/generate_db_connection.py
+++ src/fastapi_crud_generator/sqlalchemy_code_generator/generate_db_connection.py
@@ -1,5 +1,6 @@
 from pathlib import Path  # hope to move in dedicated module in the future
 import logging
+from jinja2 import Template
 
 PLACEHOLDERS = ['db_type', 'db_user', 'db_user_pass', 'db_host', 'db_port', 'db_instance']
 
@@ -28,25 +29,12 @@ def generate_connection_from_template(cfg: ConnectionConfig) -> None:
 
     :param cfg: A (data)class that stores the parameters of a DB connection.
     """
-    with open(f'{get_project_root()}/templates/db_conn.txt', 'r') as f:
-        db_conn_template = replace_all_placeholders(f.read(), cfg)
+    with open(f'{get_project_root()}/templates/db_conn.jinja2', 'r') as f:
+        db_conn_template = Template(f.read())
+        db_conn_code = db_conn_template.render(cfg=cfg)
         with open(f'{get_project_root()}/generated/db.py', 'w') as gen_f:
-            gen_f.write(db_conn_template)
+            gen_f.write(db_conn_code)
             logging.info("Successfully generated database connection code.")
-
-
-def replace_all_placeholders(generated_code: str, cfg: ConnectionConfig) -> str:
-    """
-    Method that replaces the placeholders from the database connection template text.
-
-    :param generated_code:
-    :param cfg: A (data)class that stores the parameters of a DB connection.
-    :return: a string(str) representing the equivalent Python code for the DB connection.
-    """
-    for placeholder in PLACEHOLDERS:
-        generated_code = generated_code.replace(f'{{{placeholder}}}', f'\'{str(getattr(cfg, placeholder))}\'')
-
-    return generated_code
 
 
 def generate_connection():
@@ -54,3 +42,7 @@ def generate_connection():
     Orchestrator method that triggers the generation of the database connection Python code file.
     """
     generate_connection_from_template(ConnectionConfig())
+
+
+if __name__ == "__main__":
+    generate_connection()
